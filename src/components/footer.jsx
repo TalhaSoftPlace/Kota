d950a43050b150e854286de41985a3033274d482
Talha Nazir <talhanazir410@gmail.com>
2025-04-08 16:14:13 +0500

    fix

---
+++ src/components/footer.jsx
@@ -0,0 +1,87 @@
+export const Footer = () => {
+  return (
+    <footer className="Footer_footer__pQtti">
+      <div className="container">
+        <div className="Footer_top__YBhK_">
+          <svg className="Footer_logo__fUEOF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 90">
+            <path fill="#fff" strokeWidth="0" d="M5 5h80v80H5V5zM0 90h90V0H0v90zm64.66-15.67l-5.15-16.61h-.1l-5.35 16.61H47.6l9.49-24.09h4.74l9.39 24.09h-6.56zm-39.77 0V55.67h-6.66v-5.43H37.6v5.43h-6.66v18.66h-6.06zm34.52-59.16c-3.94-.1-7.67 1.74-9.99 4.92-.64.81-1.15 1.7-1.52 2.67-.6 1.64-.9 3.38-.9 5.13v.05c.07 6.78 5.62 12.22 12.4 12.15h.56c6.88-.29 12.22-6.1 11.93-12.97-.28-6.69-5.79-11.96-12.48-11.94m.1 19.48c-3.94 0-6.56-3.38-6.56-7.18s2.62-6.97 6.46-6.97 6.46 3.38 6.46 7.07-2.52 7.08-6.36 7.08m-28.87 5.02l-6.46-10.35h-.1v10.35h-6.06v-24.1h6.06v10.87h.1l6.66-10.87h6.36l-7.87 12 8.38 12.1h-7.06z"></path>
+          </svg>
+          <a className="Footer_email__nLajh CopyText_copyText__OtWNq">hello@kota.co.uk<div className="CopyText_tooltip__HuxWa">Copy email address</div></a>
+        </div>
+        <div className="Footer_bottom__wxP7Z">
+          <div className="Footer_socialLinks__tA7gt">
+            <a href="https://www.linkedin.com/company/kota-creative/" target="_blank" rel="noopener noreferrer">LinkedIn
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.69 12.69">
+                <path fill="#fff" strokeWidth="0" d="M12.65.31a.504.504 0 00-.46-.31H.5C.22 0 0 .22 0 .5s.22.5.5.5h10.49L.55 11.43c-.2.2-.2.51 0 .71a.485.485 0 00.7 0L11.69 1.71v10.47c0 .28.22.5.5.5s.5-.22.5-.5V.52c0-.07 0-.14-.04-.21z"></path>
+              </svg>
+            </a>
+            <a href="https://www.facebook.com/kotacreative/" target="_blank" rel="noopener noreferrer">Facebook
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.69 12.69">
+                <path fill="#fff" strokeWidth="0" d="M12.65.31a.504.504 0 00-.46-.31H.5C.22 0 0 .22 0 .5s.22.5.5.5h10.49L.55 11.43c-.2.2-.2.51 0 .71a.485.485 0 00.7 0L11.69 1.71v10.47c0 .28.22.5.5.5s.5-.22.5-.5V.52c0-.07 0-.14-.04-.21z"></path>
+              </svg>
+            </a>
+            <a href="https://www.instagram.com/kotalondon/" target="_blank" rel="noopener noreferrer">Instagram
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.69 12.69">
+                <path fill="#fff" strokeWidth="0" d="M12.65.31a.504.504 0 00-.46-.31H.5C.22 0 0 .22 0 .5s.22.5.5.5h10.49L.55 11.43c-.2.2-.2.51 0 .71a.485.485 0 00.7 0L11.69 1.71v10.47c0 .28.22.5.5.5s.5-.22.5-.5V.52c0-.07 0-.14-.04-.21z"></path>
+              </svg>
+            </a>
+            <a href="https://bsky.app/profile/kotacreative.bsky.social" target="_blank" rel="noopener noreferrer">Bluesky
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.69 12.69">
+                <path fill="#fff" strokeWidth="0" d="M12.65.31a.504.504 0 00-.46-.31H.5C.22 0 0 .22 0 .5s.22.5.5.5h10.49L.55 11.43c-.2.2-.2.51 0 .71a.485.485 0 00.7 0L11.69 1.71v10.47c0 .28.22.5.5.5s.5-.22.5-.5V.52c0-.07 0-.14-.04-.21z"></path>
+              </svg>
+            </a>
+          </div>
+          <ul className="Footer_lastNav__at6Kh" data-type="mobile">
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/faqs">FAQs</a></li>
+            <li><a href="/privacy-policy">Privacy Policy</a></li>
+          </ul>
+          <div className="Footer_sectorsWrap__UM4Zx" data-type="mobile">
+            <span className="Footer_sectorTitle__yEZhb">Our sectors :</span>
+            <ul className="Footer_sectorMenu__GX7QZ">
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/agencies/">Agencies</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/b2b-tech-software-saas/">SaaS and Tech</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/b2b-transformation/">B2B Transformation</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/healthcare/">Healthcare</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/media-entertainment/">Media &amp; Entertainment</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/retail/">Retail</a>
+            </ul>
+          </div>
+          <div className="Footer_logos__jFNys">
+            <img src="https://kota-content.b-cdn.net/app/uploads/2024/06/DAN-1-500x167.png" alt="" />
+            <img src="https://kota-content.b-cdn.net/app/uploads/2023/10/clutch.svg" alt="" />
+            <img src="https://kota-content.b-cdn.net/app/uploads/2023/10/awwwards.svg" alt="" />
+            <img src="https://kota-content.b-cdn.net/app/uploads/2023/10/cssda.svg" alt="" />
+          </div>
+        </div>
+        <div className="Footer_last__auU9C">
+          <ul className="Footer_lastNav__at6Kh" data-type="desktop">
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/faqs">FAQs</a></li>
+            <li><a href="/privacy-policy">Privacy Policy</a></li>
+          </ul>
+          <a className="Footer_newsletterButton__wp2hz" href="/newsletter">Sign up to our newsletter</a>
+        </div>
+        <div className="Footer_copyright__j4atB">
+          <div className="Footer_sectorsWrap__UM4Zx" data-type="desktop">
+            <span className="Footer_sectorTitle__yEZhb">Our sectors :</span>
+            <ul className="Footer_sectorMenu__GX7QZ">
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/agencies/">Agencies</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/b2b-tech-software-saas/">SaaS and Tech</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/b2b-transformation/">B2B Transformation</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/healthcare/">Healthcare</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/media-entertainment/">Media &amp; Entertainment</a>
+              <a className="Footer_sectorItem__0Cx14" href="https://kota.co.uk/retail/">Retail</a>
+            </ul>
+          </div>
+          <div className="Footer_copyrightTextWrap__uhQWh">
+            <span className="Footer_copyrightText__f_Lte">© KOTA 2025</span>
+            <a target="_blank" href="https://registry.blockmarktech.com/certificates/7a2ea41b-bea0-427b-a497-e5ff14671428/" className="Footer_cyberEssestials__2qh89">
+              <img src="/images/cyber-essentials.svg" alt="cyber-essentials" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
